fedb0a3c8f24c43d3c7afaeb50935f5a7d87d009
Garrett <Garrett.Bodley@gmail.com>
2021-01-11 15:38:56 -0500

    commit tree 311801e43ae57880b089168b92382e75d5f9264a

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,31 @@
 Rails.application.routes.draw do
-  resources :authors, only: %i[show index] do
-    resources :posts, only: %i[show index new edit]
+  resources :authors, only: [:show, :index] do
+    resources :posts, only: [:show, :index, :new, :edit]
   end
 
-  resources :posts, only: %i[index show new create edit update]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
-  get '/admin/stats', to: 'stats#index'
+  # get '/admin/stats', to: 'stats#index'
+  # 
+  # turns into:
+  # 
+  # scope '/admin' do
+  #   resources :stats, only: [:index]
+  # end 
+  # 
+  # but this needs an associated module:
+  # 
+  # scope '/admin', module: 'admin' do
+  #   resources :stats, only: [:index]
+  # end 
+  # 
+  # this can be shortened to:
+
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
+
+  # !! However, the above code changes the route to "admin_stats_path" instead of "stats_path"
 
   root 'posts#index'
 end
